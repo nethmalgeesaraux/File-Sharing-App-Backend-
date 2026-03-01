0253ed4a7e8df1206d7df8ec5eb09010503aabd7
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-03-01 12:50:44 +0530

    ProfileService

---
+++ src/main/java/com/example/File_Sharing/Service/ProfileService.java
@@ -0,0 +1,49 @@
+package com.example.File_Sharing.Service;
+
+import com.example.File_Sharing.Documents.ProfileDocument;
+import com.example.File_Sharing.Dto.ProfileDto;
+import com.example.File_Sharing.Repository.ProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+
+    private final ProfileRepository profileRepository;
+
+    public ProfileDto createProfile(ProfileDto profileDto) {
+
+//        if (profileRepository.existsByClerkId(profileDto.getClerkId())) {
+//            throw new RuntimeException("Profile already exists for clerkId: " + profileDto.getClerkId());
+//        }
+
+        ProfileDocument profile = ProfileDocument.builder()
+                .clerkId(profileDto.getClerkId())
+                .email(profileDto.getEmail())
+                .firstname(profileDto.getFirstname())
+                .lastName(profileDto.getLastName())
+                .photoUrl(profileDto.getPhotoUrl())
+                .credits(5)
+                .createdAt(Instant.now())
+                .build();
+
+        profile = profileRepository.save(profile);
+
+        return ProfileDto.builder()
+                .id(profile.getId())
+                .clerkId(profile.getClerkId())
+                .email(profile.getEmail())
+                .firstname(profile.getFirstname())
+                .lastName(profile.getLastName())
+                .photoUrl(profile.getPhotoUrl())
+                .credits(profile.getCredits())
+                .createdAt(profile.getCreatedAt())
+                .build();
+
+    }
+
+
+}
